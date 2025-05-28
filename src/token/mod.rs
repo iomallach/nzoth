e5dcf07af995657681d2580d737d3a69d72c3a03
Alexander Butenko <a.butenko.o@gmail.com>
2025-05-28 20:12:56 +0200

    Impl display for TokenKind

--- src/token/mod.rs
+++ src/token/mod.rs
@@ -1,6 +1,8 @@
+use std::fmt::Display;
+
 use crate::source::Span;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub enum TokenKind {
     Comma,
     SingleLineComment,
@@ -19,7 +21,7 @@ pub enum TokenKind {
     Bang,
     BangEqual,
     Semicolon,
-    Colon,
+    ColonColon,
     LParen,
     RParen,
     LBrace,
@@ -34,6 +36,44 @@ pub enum TokenKind {
     KWFalse,
 }
 
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TokenKind::Comma => write!(f, ","),
+            TokenKind::SingleLineComment => write!(f, "//"),
+            TokenKind::Plus => write!(f, "+"),
+            TokenKind::PlusPlus => write!(f, "++"),
+            TokenKind::Minus => write!(f, "-"),
+            TokenKind::MinusMinus => write!(f, "--"),
+            TokenKind::Slash => write!(f, "/"),
+            TokenKind::Asterisk => write!(f, "*"),
+            TokenKind::Greater => write!(f, ">"),
+            TokenKind::GreaterEqual => write!(f, ">="),
+            TokenKind::Less => write!(f, "<"),
+            TokenKind::LessEqual => write!(f, "<="),
+            TokenKind::Equal => write!(f, "="),
+            TokenKind::EqualEqual => write!(f, "=="),
+            TokenKind::Bang => write!(f, "!"),
+            TokenKind::BangEqual => write!(f, "!="),
+            TokenKind::Semicolon => write!(f, ";"),
+            TokenKind::ColonColon => write!(f, "::"),
+            TokenKind::LParen => write!(f, "("),
+            TokenKind::RParen => write!(f, ")"),
+            TokenKind::LBrace => write!(f, "{{"),
+            TokenKind::RBrace => write!(f, "}}"),
+            TokenKind::StringLit => write!(f, "string literal"),
+            TokenKind::UnbalancedQuote => write!(f, "unbalanced quote"),
+            TokenKind::Integer => write!(f, "integer"),
+            TokenKind::Illegal => write!(f, "illegal token"),
+            TokenKind::Identifier => write!(f, "identifier"),
+            TokenKind::KWLet => write!(f, "let"),
+            TokenKind::KWTrue => write!(f, "true"),
+            TokenKind::KWFalse => write!(f, "false"),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
 pub struct Token {
     pub kind: TokenKind,
     pub span: Span,
